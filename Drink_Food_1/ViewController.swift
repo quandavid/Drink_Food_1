480cf3ff9bef748482abb5148d690d6330b62496
Quan Nguyen Dinh <quannguyendinh@Quans-MacBook-Pro.local>
2016-10-07 13:37:27 +0700

    Initial Commit

---
+++ Drink_Food_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drink_Food_1
+//
+//  Created by Quan Nguyen Dinh on 10/7/16.
+//  Copyright © 2016 Quan Nguyen Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
